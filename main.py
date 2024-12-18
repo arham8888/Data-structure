a80ff16e91d9979fc53d7fd42c3100d3cefceaaf
arham8888 <arhamhasnain428@gmail.com>
2024-12-18 21:09:30 +0500

    list

---
+++ main.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "cherry", "date", "kiwi"]  
+
+fruits[2] = "orange"  
+  
+fruits.append("mango")  
+fruits.append("pineapple")  
+fruits.remove("banana")
+fruits.insert(2,"tomato")
+print(fruits[2:1:3])
+print(fruits[5:-1])
+
+  
+print(fruits)
